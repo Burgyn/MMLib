38c3dfe597c315cd856dbe6e6552287d83a5b8bf
MinoMartiniak_cp <SND\MinoMartiniak_cp>
2013-12-12 21:15:25 +0000

    Delete

---
+++ MMLib.Extensions.Test/StringExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MMLib.Extensions;
+
+namespace MMLib.Extensions.Test
+{
+    [TestClass]
+    public class StringExtensions
+    {
+
+        [TestMethod]
+        public void RemoveDiacritics_NullAndEmptyTest()
+        {
+            string target = string.Empty;
+            Assert.AreEqual(string.Empty, target.RemoveDiacritics());
+
+            target = null;
+            Assert.AreEqual(null, target.RemoveDiacritics());
+
+
+            target = "   ";
+            Assert.AreEqual(target, target.RemoveDiacritics());
+        }
+
+        [TestMethod]
+        public void RemoveDiacritics_Test()
+        {
+            string target = "+ľščťžýáíéqwertyuiopasdfghjklô§ň&zxcvbnm,.-_:?MNBVCXZ)!LKJHGFDSA(/POIUYTREWQŠŘČŇŽÍÁÉÚ";
+            string expected = "+lsctzyaieqwertyuiopasdfghjklo§n&zxcvbnm,.-_:?MNBVCXZ)!LKJHGFDSA(/POIUYTREWQSRCNZIAEU";
+            string actual = target.RemoveDiacritics();
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
--- MMLib.Extensions/Properties/AssemblyInfo.cs
+++ MMLib.Extensions/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MMLib")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("MMLib.Extensions")]
+[assembly: AssemblyDescription("Package for string helpfull extensions")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("HP")]
-[assembly: AssemblyProduct("MMLib")]
-[assembly: AssemblyCopyright("Copyright © HP 2013")]
+[assembly: AssemblyCompany("Mino Martiniak")]
+[assembly: AssemblyProduct("MMLib.Extensions")]
+[assembly: AssemblyCopyright("Copyright © Mino Martinak  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("929152f5-e091-4768-ab62-dee6517593d3")]
+[assembly: Guid("59337cfb-fb69-41a0-a531-df4504ee092b")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.2")]
+[assembly: AssemblyFileVersion("1.0.0.2")]
