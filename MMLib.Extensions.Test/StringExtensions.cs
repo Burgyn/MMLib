e64dfb5728f0a9bb2dd930c8a7d3517eb6be534a
MinoMartiniak_cp <SND\MinoMartiniak_cp>
2013-12-13 14:17:02 +0000

    Adding new string extensions

--- MMLib.Extensions.Test/StringExtensions.cs
+++ MMLib.Extensions.Test/StringExtensions.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MMLib.Extensions;
+using System.Collections.Generic;
 
 namespace MMLib.Extensions.Test
 {
     [TestClass]
     public class StringExtensions
     {
+
+        #region Remove diacritics
 
         [TestMethod]
         public void RemoveDiacritics_NullAndEmptyTest()
@@ -31,5 +34,321 @@ namespace MMLib.Extensions.Test
 
             Assert.AreEqual(expected, actual);
         }
+
+        #endregion
+
+
+        #region Truncate Tests
+
+        [TestMethod]
+        public void Truncate_NullTest()
+        {
+            string original = null;
+            string expected = original;
+            string actual = original.Truncate(10);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod]
+        public void Truncate_StringEmptyTest()
+        {
+            string original = string.Empty;
+            string expected = original;
+            string actual = original.Truncate(10);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod]
+        public void Truncate_NoTextTest()
+        {
+            string original = "";
+            string expected = original;
+            string actual = original.Truncate(10);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod]
+        public void Truncate_LongerTextTest()
+        {
+            string original = "To tá heľpa je ale za pesnička obstarožná.";
+            string expected = "To tá heľp";
+            string actual = original.Truncate(10);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod]
+        public void Truncate_ShorterTest()
+        {
+            string original = "To tá heľpa je ale za pesnička obstarožná.";
+            string expected = original;
+            string actual = original.Truncate(200);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod]
+        public void Truncate_SameLengthTest()
+        {
+            string original = "Hello Kitty";
+            string expected = original;
+            string actual = original.Truncate(11);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod]
+        public void Truncate_ZeroLengthTest()
+        {
+            string original = "Poziarnik Sam";
+            string expected = string.Empty;
+            string actual = original.Truncate(0);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        #endregion
+
+
+        #region RemoveChars Tests
+
+        [TestMethod]
+        public void RemoveChars_NullOwnerNullTest()
+        {
+            string original = null;
+            string actual = original.RemoveChars(null);
+
+            Assert.IsNull(actual);
+        }
+
+
+        [TestMethod]
+        public void RemoveChars_NullOwnerStringEmptyTest()
+        {
+            string original = null;
+            string actual = original.RemoveChars(string.Empty);
+
+            Assert.IsNull(actual);
+        }
+
+
+        [TestMethod]
+        public void RemoveChars_NullOwnerNoTextTest()
+        {
+            string original = null;
+            string actual = original.RemoveChars("");
+
+            Assert.IsNull(actual);
+        }
+
+
+        [TestMethod]
+        public void RemoveChars_NullOwnerRemoveTest()
+        {
+            string original = null;
+            string actual = original.RemoveChars("Nothing goes happend. It is still returning null.");
+
+            Assert.IsNull(actual);
+        }
+
+
+        [TestMethod]
+        public void RemoveChars_NullTest()
+        {
+            string original = "Hello Kitty";
+            string expected = original;
+            string actual = original.RemoveChars(null);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod]
+        public void RemoveChars_StringEmptyTest()
+        {
+            string original = "Hello Kitty";
+            string expected = original;
+            string actual = original.RemoveChars(string.Empty);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod]
+        public void RemoveChars_NoTextTest()
+        {
+            string original = "Hello Kitty";
+            string expected = original;
+            string actual = original.RemoveChars("");
+
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod]
+        public void RemoveChars_RemoveTest()
+        {
+            string original = "Hel*lo <Ki/tty";
+            string expected = "Hello Kitty";
+            string actual = original.RemoveChars("*?</");
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        #endregion
+
+
+        #region Split by separator tests
+
+        [TestMethod]
+        public void SplitBySeparatorTest()
+        {
+            string value = "1;2;3;4;5;6";
+            IList<string> actual = value.SplitBySeparator(";");
+
+            Assert.AreEqual(6, actual.Count);
+            for (int i = 0; i < 6; i++)
+            {
+                Assert.AreEqual((i + 1).ToString(), actual[i]);
+            }
+        }
+
+
+        [TestMethod]
+        public void SplitBySeparatorendSeparatorTest()
+        {
+            string value = "1;2;3;4;5;6;";
+            IList<string> actual = value.SplitBySeparator(";");
+
+            Assert.AreEqual(6, actual.Count);
+            for (int i = 0; i < 6; i++)
+            {
+                Assert.AreEqual((i + 1).ToString(), actual[i]);
+            }
+        }
+
+
+        [TestMethod]
+        public void SplitBySeparatorDoubleSeparatorTest()
+        {
+            string value = "1;2;3;4;;5;6;";
+            IList<string> actual = value.SplitBySeparator(";");
+
+            Assert.AreEqual(6, actual.Count);
+            for (int i = 0; i < 6; i++)
+            {
+                Assert.AreEqual((i + 1).ToString(), actual[i]);
+            }
+        }
+
+
+        [TestMethod]
+        public void SplitBySeparator_NewLineNotDeleteEmptyLinesTest()
+        {
+            string value = "1\r\n2\r\n\r\n3\r\n";
+            IList<string> actual = value.SplitBySeparator(Environment.NewLine, false);
+
+            Assert.AreEqual(5, actual.Count);
+            Assert.AreEqual("1", actual[0]);
+            Assert.AreEqual("2", actual[1]);
+            Assert.AreEqual("", actual[2]);
+            Assert.AreEqual("3", actual[3]);
+            Assert.AreEqual("", actual[4]);
+        }
+
+
+        [TestMethod]
+        public void SplitBySeparator_NewLineDeleteEmptyLinesTest()
+        {
+            string value = "1\r\n2\r\n\r\n3\r\n";
+            IList<string> actual = value.SplitBySeparator(Environment.NewLine, true);
+
+            Assert.AreEqual(3, actual.Count);
+            Assert.AreEqual("1", actual[0]);
+            Assert.AreEqual("2", actual[1]);
+            Assert.AreEqual("3", actual[2]);
+        }
+
+
+        [TestMethod]
+        public void SplitBySeparator_NewLineTest()
+        {
+            string value = "1\r\n2\r\n\r\n3\r\n";
+            IList<string> actual = value.SplitBySeparator(Environment.NewLine);
+
+            Assert.AreEqual(3, actual.Count);
+            Assert.AreEqual("1", actual[0]);
+            Assert.AreEqual("2", actual[1]);
+            Assert.AreEqual("3", actual[2]);
+        }
+
+        #endregion
+
+
+        #region SafeTrimStart Tests
+
+        [TestMethod]
+        public void SafeTrimStartTest()
+        {
+            string value = null;
+            Assert.IsNull(value.SafeTrimStart());
+            value = string.Empty;
+            Assert.AreEqual(string.Empty, value.SafeTrimStart());
+            value = "Bilango";
+            Assert.AreEqual(value.TrimStart(), value.SafeTrimStart());
+        }
+
+        #endregion
+
+
+        #region SafeLength Tests
+
+        [TestMethod]
+        public void SafeLengthTest()
+        {
+            string value = null;
+            Assert.AreEqual(0, value.SafeLength());
+            value = string.Empty;
+            Assert.AreEqual(value.Length, value.SafeLength());
+            value = "Bilango";
+            Assert.AreEqual(value.Length, value.SafeLength());
+        }
+
+        #endregion
+
+
+        #region SafeSubstring
+
+        [TestMethod]
+        public void SafeSubstringTest()
+        {
+            string target = null;
+
+            Assert.IsNull(target.SafeSubstring(1));
+
+            target = string.Empty;
+            Assert.AreEqual(string.Empty, target.SafeSubstring(10));
+
+            target = " ";
+            Assert.AreEqual(" ", target.SafeSubstring(10));
+
+            target = "Mino";
+            Assert.AreEqual("M", target.SafeSubstring(1));
+            Assert.AreEqual("Mi", target.SafeSubstring(2));
+            Assert.AreEqual("Min", target.SafeSubstring(3));
+            Assert.AreEqual("Mino", target.SafeSubstring(4));
+            Assert.AreEqual("Mino", target.SafeSubstring(5));
+        }
+
+        #endregion
     }
 }
--- MMLib.Extensions/StringExtensions.cs
+++ MMLib.Extensions/StringExtensions.cs
@@ -12,6 +12,7 @@ namespace MMLib.Extensions
     /// </summary>
     public static class StringExtensions
     {
+
         /// <summary>
         /// Remove diacritics from string. Origin value can be null and String.Empty.
         /// </summary>
@@ -40,6 +41,170 @@ namespace MMLib.Extensions
             {
                 return value;
             }
+        }
+
+
+        /// <summary>
+        /// Truncate string to max lenght. Origin value can by null and Strin.Empty.
+        /// </summary>
+        /// <param name="value">Origin string value.</param>
+        /// <param name="maxLength">Max length of new string. Greater than zero.</param>
+        /// <returns>Truncated string.</returns>
+        public static string Truncate(this string value,
+                                              int maxLength)
+        {
+            Contract.Requires(maxLength >= 0);
+            Contract.Ensures(string.IsNullOrEmpty(Contract.Result<string>()) || Contract.Result<string>().Length <= maxLength);
+
+            string ret = value;
+
+            if (string.IsNullOrEmpty(value))
+            {
+                ret = value;
+            }
+            else
+            {
+                if (value.Length > maxLength)
+                {
+                    ret = value.Substring(0, maxLength);
+                }
+            }
+
+            return ret;
+        }
+
+
+        /// <summary>
+        /// Remove unexpected chars from string.
+        /// </summary>
+        /// <param name="value">Origin string value. Can by null and String.Empty.</param>
+        /// <param name="unexpectedCharsString">String with unexpected chars. Can by null and String.Empty.</param>
+        /// <returns>New string without unexpected chars</returns>
+        public static string RemoveChars(this string value,
+                                              string unexpectedCharsString)
+        {
+            string ret = value;
+
+            if (!(string.IsNullOrEmpty(value) || string.IsNullOrEmpty(unexpectedCharsString)))
+            {
+                foreach (var item in unexpectedCharsString)
+                {
+                    var str = item.ToString();
+                    if (!string.IsNullOrEmpty(str))
+                    {
+                        ret = ret.Replace(str, string.Empty);
+                    }
+                }
+            }
+
+            return ret;
+        }
+
+
+        /// <summary>
+        /// Split string by string separator.
+        /// </summary>
+        /// <param name="value">Origin string.</param>
+        /// <param name="separator">Separator for spliting origin string.</param>
+        /// <returns>
+        /// Splited by separator.
+        /// </returns>
+        public static IList<string> SplitBySeparator(this string value,
+                                                          string separator)
+        {
+            return SplitBySeparator(value, separator, true);
+        }
+
+
+        /// <summary>
+        /// Split string by string separator.
+        /// </summary>
+        /// <param name="value">Origin string.</param>
+        /// <param name="separator">Separator for spliting origin string.</param>
+        /// <param name="canRemoveEmptyLines">
+        /// True, if can remove empty lines. Else false.
+        /// </param>
+        /// <returns>
+        /// Splited by separator.
+        /// </returns>
+        public static IList<string> SplitBySeparator(this string value,
+                                                          string separator,
+                                                            bool canRemoveEmptyLines)
+        {
+            List<string> ret = new List<string>();
+
+            if (!string.IsNullOrEmpty(value))
+            {
+                var splitOptions = canRemoveEmptyLines ? StringSplitOptions.RemoveEmptyEntries : StringSplitOptions.None;
+                ret = value.Split(new string[] { separator }, splitOptions).ToList();
+            }
+
+            return ret;
+        }
+
+
+        /// <summary>
+        /// Removes all leading occurrences of a set of characters specified in an array from the current System.String object.
+        /// If value is null then return null.
+        /// </summary>
+        /// <param name="value">
+        /// Origin string value.
+        /// </param>
+        /// <returns>
+        /// The string that remains after all occurrences of characters in the trimChars
+        /// parameter are removed from the start of the current string. If trimChars
+        /// is null or an empty array, white-space characters are removed instead.
+        /// </returns>
+        public static string SafeTrimStart(this string value)
+        {
+            return value == null ? value : value.TrimStart();
+        }
+
+
+        /// <summary>
+        /// Gets the number of characters in the current System.String object.
+        /// If value is null then return 0.
+        /// </summary>
+        /// <param name="value">Origin string value.</param>
+        /// <returns>The number of characters in the current string.</returns>
+        public static int SafeLength(this string value)
+        {
+            return value == null ? 0 : value.Length;
+        }
+
+
+        /// <summary>
+        /// Retrieves a substring from this instance. The substring starts at a specified
+        /// character position and has a specified length.
+        /// If value is null or empty then return origin text.
+        /// </summary>
+        /// <param name="value">Origin string value.</param>
+        /// <param name="length">The number of characters in the substring.</param>
+        /// <returns>
+        /// A string that is equivalent to the substring of length length that begins
+        /// at startIndex in this instance, or System.String.Empty if startIndex is equal
+        /// to the length of this instance and length is zero.
+        /// </returns>
+        public static string SafeSubstring(this string value,
+                                                   int length)
+        {
+            Contract.Requires(length > 0);
+
+            Contract.Assert(length > 0);
+            string ret = value;
+            var len = value.SafeLength();
+
+            if (len > 0 && length < len)
+            {
+                ret = value.Substring(0, length);
+            }
+
+            if (string.IsNullOrEmpty(ret))
+            {
+                ret = value;
+            }
+
+            return ret;
         }
     }
 }
