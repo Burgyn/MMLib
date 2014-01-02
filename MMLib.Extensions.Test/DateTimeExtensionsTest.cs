64e05d27ca9dd1ef875bccdef46c256cbd9ce824
MinoMartiniak_cp <SND\MinoMartiniak_cp>
2014-01-02 11:04:41 +0000

    Add first datetime extension

---
+++ MMLib.Extensions.Test/DateTimeExtensionsTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MMLib.Extensions;
+
+namespace MMLib.Extensions.Test
+{
+    [TestClass]
+    public class DateTimeExtensionsTest
+    {
+        #region IsWeekend
+
+        [TestMethod]
+        public void IsWeekend_TrueTest()
+        {
+            DateTime target = new DateTime(2014, 1, 4);
+            Assert.IsTrue(target.IsWeekend());
+
+            target = new DateTime(2014, 1, 5);
+            Assert.IsTrue(target.IsWeekend());
+
+            target = new DateTime(2013, 12, 28);
+            Assert.IsTrue(target.IsWeekend());
+
+            target = new DateTime(2013, 12, 29);
+            Assert.IsTrue(target.IsWeekend());
+        }
+
+
+        [TestMethod]
+        public void IsWeekend_FalseTest()
+        {
+            DateTime target = new DateTime(2014, 1, 3);
+            Assert.IsFalse(target.IsWeekend());
+
+            target = new DateTime(2014, 1, 6);
+            Assert.IsFalse(target.IsWeekend());
+
+            target = new DateTime(2014, 1, 9);
+            Assert.IsFalse(target.IsWeekend());
+
+            target = new DateTime(2012, 9, 12);
+            Assert.IsFalse(target.IsWeekend());
+
+            target = new DateTime(2015, 8, 5);
+            Assert.IsFalse(target.IsWeekend());
+        } 
+
+        #endregion
+    }
+}
---
+++ MMLib.Extensions/DateTimeExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+
+namespace MMLib.Extensions
+{
+    /// <summary>
+    /// Extensions for DataTime class.
+    /// </summary>
+    public static class DateTimeExtensions
+    {
+        /// <summary>
+        /// Determine whether date is weekend.
+        /// </summary>
+        /// <param name="value">Date, which want tested.</param>
+        /// <returns>true if date is in weekend, otherwise false.</returns>
+        public static bool IsWeekend(this DateTime value)
+        {
+            Contract.Requires(value != null);
+
+            return value.DayOfWeek == DayOfWeek.Sunday || value.DayOfWeek == DayOfWeek.Saturday;
+        }
+    }
+}
