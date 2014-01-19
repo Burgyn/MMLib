1d7014061644651bea7f58b7d6ceaa91b6514f89
MinoMartiniak_cp <SND\MinoMartiniak_cp>
2014-01-19 16:14:34 +0000

    Adding relayCommand

---
+++ MMLib.MVVM.Test/Command/RelayCommandTest.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MMLib.MVVM.Command;
+
+namespace MMLib.MVVM.Test.Command
+{
+    [TestClass]
+    public class RelayCommandTest
+    {
+        [TestMethod]
+        public void Execute_DefaultTest()
+        {
+            bool wasCalled = false;
+            RelayCommand target = new RelayCommand((p) => {
+                wasCalled = true;
+            });
+            Assert.IsFalse(wasCalled);
+            target.Execute();
+
+            Assert.IsTrue(wasCalled);
+        }
+
+        [TestMethod]
+        public void Execute_ParamTest()
+        {
+            string value = string.Empty;
+            RelayCommand target = new RelayCommand((p) =>
+            {
+                value = p.ToString();
+            });
+
+            Assert.AreEqual(string.Empty, value);
+            target.Execute("Test");
+
+            Assert.AreEqual("Test", value);
+        }
+
+        [TestMethod]
+        public void CanExecute_DefaultTest()
+        {
+            RelayCommand target = new RelayCommand((p) =>
+            {
+            });
+            
+            Assert.IsTrue(target.CanExecute(null));
+        }
+
+        [TestMethod]
+        public void CanExecute_MethodTest()
+        {
+            bool canExecute = true;
+            RelayCommand target = new RelayCommand((p) =>
+            {
+            }, (p) => { return canExecute; });
+
+            Assert.IsTrue(target.CanExecute(null));
+
+            canExecute = false;
+            Assert.IsFalse(target.CanExecute(null));
+
+            canExecute = true;
+            Assert.IsTrue(target.CanExecute(null));
+        }
+
+        [TestMethod]
+        public void CanExecute_ParamTest()
+        {
+            RelayCommand target = new RelayCommand((p) =>
+            {
+            }, (p) => { return (bool)p; });
+
+            Assert.IsTrue(target.CanExecute(true));
+
+            Assert.IsFalse(target.CanExecute(false));
+
+            Assert.IsTrue(target.CanExecute(true));
+        }
+
+
+        [TestMethod]
+        public void Execute_CannotExecutTest()
+        {
+            var value = "test";
+            bool canExecute = true;
+            RelayCommand target = new RelayCommand((p) =>
+            {
+                value = p.ToString();
+            }, (p) => { return canExecute; });
+
+            target.Execute("mino");
+            Assert.AreEqual("mino", value);
+
+            value = string.Empty;
+            canExecute = false;
+
+            target.Execute("mino");
+            Assert.AreEqual(string.Empty, value);
+        }
+    }
+}
---
+++ MMLib.MVVM/Command/RelayCommand.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+
+namespace MMLib.MVVM.Command
+{
+    /// <summary>
+    /// Class represent command whose purpose is to relay its functionality to other objects.
+    /// </summary>
+    public class RelayCommand : ICommand
+    {
+
+        #region Private Fields
+
+        private Action<object> _execute;
+        private Predicate<object> _canExecute;
+
+        #endregion
+
+
+        #region Constructors
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="execute">Execution method delegate.</param>
+        public RelayCommand(Action<object> execute)
+            : this(execute, null)
+        {
+        }
+
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="execute">Execution method delegate.</param>
+        /// <param name="canExecute">Function for can execute logic.</param>
+        public RelayCommand(Action<object> execute,
+                         Predicate<object> canExecute)
+        {
+            Contract.Requires(execute != null);
+
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        #endregion
+
+
+        #region ICommand Members
+
+        /// <summary>
+        /// Defines the method that determines whether the command can execute in its current state.
+        /// </summary>
+        /// <param name="parameter">
+        /// Data used by the command. If the command does not require data to be passed, his object can be set to null.
+        /// </param>
+        /// <returns>true if this command can be executed; otherwise, false.</returns>
+        [DebuggerStepThrough]
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null ? true : _canExecute(parameter);
+        }
+
+
+        /// <summary>
+        /// Defines the method to be called when the command is invoked.
+        /// </summary>
+        public void Execute()
+        {
+            OnExecute(null);
+        }
+
+
+        /// <summary>
+        /// Defines the method to be called when the command is invoked.
+        /// </summary>
+        /// <param name="parameter">Data used by the command. If the command does not require data to be passed, this object can be set to null.
+        /// </param>
+        public void Execute(object parameter)
+        {
+            OnExecute(parameter);
+        }
+
+        #endregion
+
+
+        #region Protected
+
+
+        /// <summary>
+        /// Defines the method to be called when the command is invoked.
+        /// </summary>
+        /// <param name="parameter">
+        /// Data used by the command. If the command does not require data to be passed, this object can be set to null.
+        /// </param>
+        protected virtual void OnExecute(object parameter)
+        {
+            if (this.CanExecute(parameter))
+            {
+                _execute(parameter); 
+            }
+        }
+
+
+        #endregion
+
+
+        /// <summary>
+        /// Eventa či je možné vykonať akciu.
+        /// <para>
+        /// Toto si volá WPF samé.
+        /// </para>
+        /// </summary>
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+    }
+}
