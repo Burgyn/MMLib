ea26d1388ac7ec6581f6c6aef4a08988f6dabc16
MinoMartiniak_cp <SND\MinoMartiniak_cp>
2014-01-29 20:44:43 +0000

    Extensions for object (Get and set proeprty value)

---
+++ MMLib.Extensions.Test/ObjectExtensionsTest.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MMLib.Extensions;
+
+namespace MMLib.Extensions.Test
+{
+    [TestClass]
+    public class ObjectExtensionsTest
+    {
+        [TestMethod]
+        public void SetPropertyValue_Test()
+        {
+            DateTime now = DateTime.Now;
+
+            TestClass target = new TestClass();
+            Assert.IsTrue(target.SetPropertyValue("IntValue", 1));
+            Assert.IsTrue(target.SetPropertyValue("StringValue", "Hello"));
+            Assert.IsTrue(target.SetPropertyValue("DoubleValue", 12));
+            Assert.IsTrue(target.SetPropertyValue("DateTimeValue", now));
+
+            Assert.IsFalse(target.SetPropertyValue("Drttt", now));
+
+            Assert.AreEqual(1, target.IntValue);
+            Assert.AreEqual(12, target.DoubleValue);
+            Assert.AreEqual("Hello", target.StringValue);
+            Assert.AreEqual(now, target.DateTimeValue);
+        }
+
+        [TestMethod]
+        public void GetPropertyValue_Test()
+        {
+            DateTime now = DateTime.Now;
+
+            TestClass target = new TestClass()
+            {
+                DateTimeValue = now,
+                DoubleValue = 14,
+                IntValue = 11,
+                StringValue = "Hello"
+            };
+
+            Assert.AreEqual(now, target.GetPropertyValue<DateTime>("DateTimeValue"));
+            Assert.AreEqual(14, target.GetPropertyValue<double>("DoubleValue"));
+            Assert.AreEqual(11, target.GetPropertyValue<int>("IntValue"));
+            Assert.AreEqual("Hello", target.GetPropertyValue<string>("StringValue"));
+        }
+
+        [TestMethod]
+        public void GetPropertyValue_InvalidPropertyNameTest()
+        {
+            DateTime now = DateTime.Now;
+
+            TestClass target = new TestClass();
+
+            try
+            {
+                var value = target.GetPropertyValue<int>("23243");
+                Assert.Fail("It must faild");
+            }
+            catch (ArgumentException)
+            {
+                //Ok
+            }
+        }
+    }
+
+
+    public class TestClass
+    {
+        public int IntValue { get; set; }
+
+        public string StringValue { get; set; }
+
+        public double DoubleValue { get; set; }
+
+        public DateTime DateTimeValue { get; set; }
+    }
+}
---
+++ MMLib.Extensions/ObjectExtensions.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+
+namespace MMLib.Extensions
+{
+    /// <summary>
+    /// Extensions for object.
+    /// </summary>
+    public static class ObjectExtensions
+    {
+
+        /// <summary>
+        /// Sets the value of the specified property.
+        /// </summary>
+        /// <param name="target">Target object.</param>
+        /// <param name="propertyName">A value which specifies the property's name.</param>
+        /// <param name="newValue">An object which represents the new value</param>
+        /// <returns>true if target object has this property, otherwise false.</returns>
+        /// <exception cref="ArgumentException">
+        /// Occured when newValue doesn't have same type as property.
+        /// </exception>
+        public static bool SetPropertyValue(this object target, string propertyName, object newValue)
+        {
+            Contract.Requires(target != null);
+            Contract.Requires(!string.IsNullOrEmpty(propertyName));
+
+            bool ret = false;
+            var targetProperty = target.GetType().GetProperty(propertyName);
+
+            if (targetProperty != null)
+            {
+                targetProperty.SetValue(target, newValue, null);
+                ret = true;
+            }
+
+            return ret;
+        }
+
+        /// <summary>
+        /// Returns the value of the specified property.
+        /// </summary>
+        /// <typeparam name="TValue">Type of return value, which expect.</typeparam>
+        /// <param name="target">Target object.</param>
+        /// <param name="propertyName">A value which specifies the property's name.</param>
+        /// <returns>
+        /// Value which represents the value of the specified property. 
+        /// </returns>
+        /// <exception cref="InvalidOperationException">
+        /// This exception occured, when target object doesn't have specific propertyName.
+        /// </exception>
+        /// <exception cref="System.ArgumentException">
+        /// This exception occured, when return type is not type of the TValue.
+        /// </exception>
+        public static TValue GetPropertyValue<TValue>(this object target, string propertyName)
+        {
+            TValue ret = default(TValue);
+            Contract.Requires(target != null);
+            Contract.Requires(!string.IsNullOrEmpty(propertyName));
+
+            var targetProperty = target.GetType().GetProperty(propertyName);
+
+            if (targetProperty != null)
+            {
+                ret = (TValue)targetProperty.GetValue(target, null);
+            }
+            else
+            {
+                throw new ArgumentException("Target object doesn't have property:'{0}'".FormatAsPattern(propertyName));
+            }
+
+            return ret;
+        }
+    }
+}
