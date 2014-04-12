b11ee00877c12161e10eaaabaee258f1137dc7b0
MinoMartiniak_cp <SND\MinoMartiniak_cp>
2014-04-12 14:49:07 +0000

    Adding Minus converter

---
+++ MMLib.WPF.Test/Converters/MinusDoubleConverterTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MMLib.WPF.Converters;
+
+namespace MMLib.WPF.Test.Converters
+{
+    [TestClass]
+    public class MinusDoubleConverterTest
+    {
+        [TestMethod]
+        public void BaseTest()
+        {
+            MinusDoubleConverter target = new MinusDoubleConverter();
+            double oreginalValue = 22;
+            double parameterValue = 12;
+            double expected = 10;
+
+            Assert.AreEqual(expected, target.Convert(oreginalValue, null, parameterValue, null));
+
+            oreginalValue = 42.5;
+            parameterValue = 9;
+            expected = 33.5;
+
+            Assert.AreEqual(expected, target.Convert(oreginalValue, null, parameterValue, null));
+        }
+    }
+}
---
+++ MMLib.WPF/Converters/MinusDoubleConverter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace MMLib.WPF.Converters
+{
+    /// <summary>
+    /// Converter, which convert double value to double minus specific value.
+    /// </summary>
+    public class MinusDoubleConverter : IValueConverter
+    {
+        /// <summary>
+        /// Convert double value to double minus specific value (converter parameter).
+        /// </summary>
+        /// <param name="value">Oreginal double value.</param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter">Double value, which specified value which we want remove from oreginal value.</param>        
+        /// <param name="culture"></param>
+        /// <returns>Double value minus parameter value.</returns>
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            Contract.Requires(value != null);
+            Contract.Requires(targetType != null);
+
+            double oreginalValue = (double)value;
+            double parameterValue = (double)parameter;
+
+            return oreginalValue - parameterValue;
+        }
+
+        /// <summary>
+        /// NotImplementedException.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
